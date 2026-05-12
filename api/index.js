de404a378ecffb30d08302232982a3a18fffc1c3
Ramonprojects <adm@ramonmago.com>
2026-05-12 18:02:58 -0300

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,4 @@
+  export default function handler(req, res) {                                                                                                                                              
+    res.writeHead(302, { Location: 'https://rbingo.sugestao.cc/' });
+    res.end();                                                                                                                                                                             
+  }
