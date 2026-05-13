fdb894240fb2b3ec53fb5b418981dc311f38fc63
Ramonprojects <adm@ramonmago.com>
2026-05-13 10:12:27 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=NF4J2KXLUFC&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=SVIW86TWW0Bafp=bm' });
     res.end();
   }
