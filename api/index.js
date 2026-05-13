fdb8f3147ca8765f648cd0df04f5c878baec85c7
Ramonprojects <adm@ramonmago.com>
2026-05-13 11:17:40 -0300

    Fix URL query parameter in redirect response

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=SVIW86TWW0Bafp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=SVIW86TWW0B&afp=bm' });
     res.end();
   }
