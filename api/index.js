d4585e36d7e863c8481545f2772de0fcc90898ff
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-16 09:35:49 -0300

    chore: update shareCode to LUO3EDK422O

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=MVLAX1BZRUP&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=LUO3EDK422O&afp=bm' });
     res.end();
   }
