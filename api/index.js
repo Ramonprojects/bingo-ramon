5d2a9156dd7b5447921bbeca4ee24e4fde9b8090
Ramonprojects <272552832+Ramonprojects@users.noreply.github.com>
2026-05-17 09:52:52 -0300

    chore: update shareCode to AT7UO7PV6TP

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=LUO3EDK422O&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/5yry3v7n?shareCode=AT7UO7PV6TP&afp=bm' });
     res.end();
   }
