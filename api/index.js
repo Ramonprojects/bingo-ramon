76322d271db4cd9575b06e64b8993de3633b035c
Ramonprojects <adm@ramonmago.com>
2026-06-11 15:18:42 -0300

    chore: shareCode L1LWDFF8G9Q -> AV1BPEXY133 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=L1LWDFF8G9Q&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=AV1BPEXY133&afp=bm' });
     res.end();
   }
