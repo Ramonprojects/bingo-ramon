5bf7c6f4f25e888f5e35fa28b0d09a6ff65c6bfd
Ramonprojects <adm@ramonmago.com>
2026-06-14 09:32:19 -0300

    chore: shareCode IMFD95LZ85J -> N2RIUZLCKJE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=IMFD95LZ85J&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=N2RIUZLCKJE&afp=bm' });
     res.end();
   }
