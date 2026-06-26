bd954ab585c5a97641b03059822c2c572c0341d9
Ramonprojects <adm@ramonmago.com>
2026-06-26 10:26:15 -0300

    chore: shareCode 08QQJTRJZAQ -> Z7KUQVLU8KH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=08QQJTRJZAQ&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=Z7KUQVLU8KH&afp=bm' });
     res.end();
   }
