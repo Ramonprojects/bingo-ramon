49deac6c3bdc05c42636565420697058ab773f46
Ramonprojects <adm@ramonmago.com>
2026-07-07 09:42:01 -0300

    chore: shareCode R7HVWQVJ3I3 -> UVKF2TMWKF6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=R7HVWQVJ3I3&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=UVKF2TMWKF6&afp=bm' });
     res.end();
   }
