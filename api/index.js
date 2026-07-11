a2d060c259755ba42d911f505abbc6877ff1074c
Ramonprojects <adm@ramonmago.com>
2026-07-11 10:02:44 -0300

    chore: shareCode 61G9U74P6UT -> B6UF37UIQK6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=61G9U74P6UT&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=29514&shareCode=B6UF37UIQK6&afp=bm' });
     res.end();
   }
