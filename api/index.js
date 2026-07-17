f65a5fe229a89b2b97b19e9a3e8749c4382df2f8
Ramonprojects <adm@ramonmago.com>
2026-07-17 14:14:26 -0300

    chore: shareCode NKKRLOSDSBP -> SBI7V7LF551 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=NKKRLOSDSBP&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=SBI7V7LF551&afp=bm' });
     res.end();
   }
