60e5e9a6f06d897c419b3d9197d811b3e29061bb
Ramonprojects <adm@ramonmago.com>
2026-07-21 12:06:20 -0300

    chore: shareCode 264MS0RU4GB -> ALSAHPQMG1V (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=264MS0RU4GB&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=ALSAHPQMG1V&afp=bm' });
     res.end();
   }
