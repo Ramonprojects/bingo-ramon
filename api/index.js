245ebc9e5cd5d9f304655bbd2dee258fc26f38aa
Ramonprojects <adm@ramonmago.com>
2026-07-22 10:46:48 -0300

    chore: shareCode ALSAHPQMG1V -> WKEH6OPQXA6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=ALSAHPQMG1V&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=WKEH6OPQXA6&afp=bm' });
     res.end();
   }
