0a73c6d73464442f4a24207bdfeb630a0ad31be1
Ramonprojects <adm@ramonmago.com>
2026-07-25 12:24:23 -0300

    chore: shareCode R9980TZRBDW -> ASWWVALDVW1 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=R9980TZRBDW&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=ASWWVALDVW1&afp=bm' });
     res.end();
   }
