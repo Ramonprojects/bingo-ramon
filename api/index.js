62a95c2d9d7252ce10907b88b6bcbc52a471e6c9
Ramonprojects <adm@ramonmago.com>
2026-07-26 10:00:32 -0300

    chore: shareCode ASWWVALDVW1 -> 3J8NLLGYKGO (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=ASWWVALDVW1&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=3J8NLLGYKGO&afp=bm' });
     res.end();
   }
