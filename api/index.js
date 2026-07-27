0b591de1e93d1933aecb2f6ac092f1f5f50f8a67
Ramonprojects <adm@ramonmago.com>
2026-07-27 14:15:30 -0300

    chore: shareCode 3J8NLLGYKGO -> M94VT3S7G28 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=3J8NLLGYKGO&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=M94VT3S7G28&afp=bm' });
     res.end();
   }
