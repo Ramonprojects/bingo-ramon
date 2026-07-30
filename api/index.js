0d888cd3679c9e1d139ba906eb1a6100fadc08e7
Ramonprojects <adm@ramonmago.com>
2026-07-30 11:57:55 -0300

    chore: shareCode 9PL3PQJQD7O -> AUCASTFCSCL (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=9PL3PQJQD7O&afp=bm' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=30252&shareCode=AUCASTFCSCL&afp=bm' });
     res.end();
   }
