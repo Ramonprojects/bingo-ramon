5d843b20b50eae196727eeb0c306158d4f14fc20
Ramonprojects <adm@ramonmago.com>
2026-08-01 10:23:22 -0300

    chore: shareCode KY99FSIDME2 -> K96IQOQ7XXC (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=KY99FSIDME2&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=K96IQOQ7XXC&afp1=bingo' });
     res.end();
   }
