e20e2a1e761ffc3cc4daeefed1db42056d817689
Ramonprojects <adm@ramonmago.com>
2026-08-11 14:06:41 -0300

    chore: shareCode 9B9OKDUTV5I -> MD30XOT49W8 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=9B9OKDUTV5I&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=MD30XOT49W8&afp1=bingo' });
     res.end();
   }
