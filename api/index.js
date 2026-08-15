be4106acf38d676411847e99436fcc484bc4093b
Ramonprojects <adm@ramonmago.com>
2026-08-15 11:58:28 -0300

    chore: shareCode 5DRPXJFPMKA -> BS6I6AI8QDG (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=5DRPXJFPMKA&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=BS6I6AI8QDG&afp1=bingo' });
     res.end();
   }
