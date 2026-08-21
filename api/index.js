8c25cd45f4601a3bf9e16861bd6b77827553b236
Ramonprojects <adm@ramonmago.com>
2026-08-21 12:45:14 -0300

    chore: shareCode MSMOYPO8S5G -> 86N7CM8D377 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=MSMOYPO8S5G&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=86N7CM8D377&afp1=bingo' });
     res.end();
   }
