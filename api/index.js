7ba544a5e9041f1d530060ef1af5436fd29a20b5
Ramonprojects <adm@ramonmago.com>
2026-08-23 11:54:53 -0300

    chore: shareCode AJWLPDHKPIV -> N95TIE1OMZR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=AJWLPDHKPIV&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=N95TIE1OMZR&afp1=bingo' });
     res.end();
   }
