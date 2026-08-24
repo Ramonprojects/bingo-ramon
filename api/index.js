12a719b90cb688b0b163917e70605c590f6b723b
Ramonprojects <adm@ramonmago.com>
2026-08-24 12:29:20 -0300

    chore: shareCode N95TIE1OMZR -> C11V35GI5CW (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=N95TIE1OMZR&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=C11V35GI5CW&afp1=bingo' });
     res.end();
   }
