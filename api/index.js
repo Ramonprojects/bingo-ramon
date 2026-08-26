a270ebace00aa77cd9ba5c92d5fc1c5b219dc354
Ramonprojects <adm@ramonmago.com>
2026-08-26 12:34:13 -0300

    chore: shareCode URVO3974TTP -> A989AXX7X14 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=URVO3974TTP&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=A989AXX7X14&afp1=bingo' });
     res.end();
   }
