c9c68d038b9672aeaa99ebbaf2451e6b01ddaa74
Ramonprojects <adm@ramonmago.com>
2026-08-27 12:53:39 -0300

    chore: shareCode A989AXX7X14 -> 49KRULCU8G2 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=A989AXX7X14&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=49KRULCU8G2&afp1=bingo' });
     res.end();
   }
