18b181292fa5978cbdf37e61d54b29ada96ad02a
Ramonprojects <adm@ramonmago.com>
2026-09-03 13:16:23 -0300

    chore: shareCode 7KZMIUI9IVV -> 9M1UMGENB6Z (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=7KZMIUI9IVV&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=9M1UMGENB6Z&afp1=bingo' });
     res.end();
   }
