f7fc27e17512477be3dd71edb9482fbe97c5455b
Ramonprojects <adm@ramonmago.com>
2026-09-05 10:06:03 -0300

    chore: shareCode Y5S5LMZIMTN -> 7UYU3DCHQNH (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=Y5S5LMZIMTN&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=7UYU3DCHQNH&afp1=bingo' });
     res.end();
   }
