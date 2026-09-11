690e7530a06fc422beb47e04045ddf7e48bd5a83
Ramonprojects <adm@ramonmago.com>
2026-09-11 13:40:57 -0300

    chore: shareCode 0Y88G66UD7O -> 2G3DL251AKE (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=0Y88G66UD7O&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=2G3DL251AKE&afp1=bingo' });
     res.end();
   }
