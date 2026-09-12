2f4925298b0cc3dfb26a5f8aad5f20401fe84f85
Ramonprojects <adm@ramonmago.com>
2026-09-12 09:20:54 -0300

    chore: shareCode 2G3DL251AKE -> VF1382F0WXP (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=2G3DL251AKE&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VF1382F0WXP&afp1=bingo' });
     res.end();
   }
