6556454787d5ec0c49fb33fa9859faf6d3574b8e
Ramonprojects <adm@ramonmago.com>
2026-09-13 09:16:11 -0300

    chore: shareCode VF1382F0WXP -> EVF4H9936TR (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=VF1382F0WXP&afp1=bingo' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/nvgf8026?campaign_id=34292&shareCode=EVF4H9936TR&afp1=bingo' });
     res.end();
   }
